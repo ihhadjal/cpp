89490088c6dde7bc31d3d33bc03a14b5b1c4f7ad
ihhadjal <ihebhdj42@gmail.com>
2025-07-10 22:38:19 +0200

    setter and getter compris vitte f

--- cpp_0/ex01/phonebook.hpp
+++ cpp_0/ex01/phonebook.hpp
@@ -7,7 +7,8 @@
 class Contact
 {
 	public:
-
+	void	setFirstName(std::string firstname);
+	std::string	getFirstName();
 	private:
 	std::string m_firstName;
 	std::string m_lastName;
@@ -19,7 +20,7 @@ class Contact
 class PhoneBook
 {
 	public:
-	void addContact(std::string str);
+	void addFirstName(std::string str);
 	private:
 	Contact m_contacts[8];
 };
