fe4e00000ff4bac4b3646a818445f127e586caa6
ihhdj <ihebhdj42@gmail.com>
2025-07-14 20:28:35 +0200

    errors fixed

--- cpp_0/ex00/megaphone.cpp
+++ cpp_0/ex00/megaphone.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 {
     char up_char;
     if (argc == 1)
-        std :: cout << "* LOUD AND UNBEREABLE FEEDBACK NOISE *\n";
+        std :: cout << "* LOUD AND UNBEREABLE FEEDBACK NOISE *";
     for (int i = 1; argv[i]; i++)
     {
         for (int j = 0; argv[i][j]; j++)
