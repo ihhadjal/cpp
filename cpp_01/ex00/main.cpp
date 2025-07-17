d8c925d9635191cfd2326f1b479db87e38a2a9e3
ihhdj <ihebhdj42@gmail.com>
2025-07-17 13:08:55 +0200

    ok

--- cpp_01/ex00/main.cpp
+++ cpp_01/ex00/main.cpp
@@ -4,10 +4,20 @@
 int main()
 {
     Zombie *newZ = newZombie("zombie 1");
+    Zombie *newZ1 = newZombie("zombie 2");
+    Zombie *newZ2 = newZombie("zombie 3");
+    Zombie *newZ3 = newZombie("zombie 4");
 
     newZ->announce();
-    randomChump("zombie 2");
+    newZ1->announce();
+    newZ2->announce();
+    newZ3->announce();
+
+    randomChump("zombie 5");
     
     delete newZ;
+    delete newZ1;
+    delete newZ2;
+    delete newZ3;
     return 0;
 }
