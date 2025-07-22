7d6006751d0676822d81fae801e1c2a9c936db92
ihhdj <ihebhdj42@gmail.com>
2025-07-22 12:41:55 +0200

    ex03 done

--- cpp_01/ex03/Weapon.hpp
+++ cpp_01/ex03/Weapon.hpp
@@ -8,4 +8,14 @@
 #include <iomanip>
 
 
+class Weapon
+{
+    private:
+    std::string m_type;
+
+    public:
+    Weapon(std::string weaponTy);
+    std::string const &geType();
+    void              setType(std::string type);
+};
 #endif
