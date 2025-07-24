fb6133596e9ea38ff76fc3c9faa1722d03c1468e
ihhdj <ihebhdj42@gmail.com>
2025-07-24 12:58:09 +0200

    ex05 done

---
+++ cpp_01/ex05/Harl.hpp
@@ -0,0 +1,23 @@
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <iomanip>
+#include <fstream>
+#include <ostream>
+
+class Harl
+{
+    private:
+    void    debug(void);
+    void    info(void);
+    void    warning(void);
+    void    error(void);
+
+    public:
+    void    complain(std::string level);
+};
+#endif
---
+++ cpp_01/ex05/main.cpp
@@ -0,0 +1,12 @@
+#include "Harl.hpp"
+
+int main()
+{
+    Harl bot;
+
+    bot.complain("DEBUG");
+    bot.complain("INFO");
+    bot.complain("WARNING");
+    bot.complain("ERROR");
+
+}
