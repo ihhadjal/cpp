223d72ca5dec098cc83449b8340a7984bb45a938
ihhdj <ihebhdj42@gmail.com>
2025-07-28 16:20:34 +0200

    first exercice done

---
+++ cpp_02/ex00/Fixed.hpp
@@ -0,0 +1,24 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <iomanip>
+
+class Fixed
+{
+    private:
+    int _fixedValue;
+    static const int _bits;
+
+    public:
+    Fixed(void);
+    Fixed(Fixed const &source);
+    ~Fixed(void);
+    Fixed &operator=(Fixed const &rhs);
+    int getRawBits(void)const;
+    void setRawBits(int const raw);
+};
+#endif
