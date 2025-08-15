b876e4d2289d26d213058f8118b35d079a9a85d8
ihhdj <ihebhdj42@gmail.com>
2025-08-15 10:05:07 +0200

    getting there

---
+++ cpp_05/ex01/Bureaucrat.hpp
@@ -0,0 +1,47 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <exception>
+#include <stdlib.h>
+class Bureaucrat
+{
+    private:
+    const std::string _name;
+    int               _grade;
+    
+    public:
+    Bureaucrat();
+    Bureaucrat(const Bureaucrat &src);
+    Bureaucrat(const std::string name, int grade);
+    ~Bureaucrat() throw();
+    Bureaucrat &operator=(const Bureaucrat &rhs);
+    
+    std::string getName()const;
+    int         getGrade()const;
+    void        incrementGrade(int points);
+    void        decrementGrade(int points);
+
+    class GradeTooLowException : public std::exception
+    {
+        private:
+        const char * _msg;
+
+        public:
+        GradeTooLowException(const char *msg);
+        const char *what() const throw();
+    };
+
+    class GradeTooHighException : public std::exception
+    {
+        private:
+        const char * _msg;
+
+        public:
+        GradeTooHighException(const char *msg);
+        const char *what() const throw();
+    };
+};
+
+std::ostream &operator<<(std::ostream &o, const Bureaucrat &i);
+#endif
