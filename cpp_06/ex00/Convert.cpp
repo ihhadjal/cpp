789ce67e56072c6feb08dd3bac4b7b8adbd6e946
ihhdj <ihebhdj42@gmail.com>
2025-08-21 19:40:06 +0200

    to be continued...

--- cpp_06/ex00/Convert.cpp
+++ cpp_06/ex00/Convert.cpp
@@ -3,6 +3,8 @@
 void ScalarConvert::convert(std::string toConvert)
 {
     int Ivalue = atoi(toConvert.c_str());
+    char *endp;
+    float conv = std::strtof(toConvert.c_str(), &endp);
 
     if ((toConvert[0] >= 33 && toConvert[0] <= 126) && toConvert.size() == 1 
         && check_if_num(toConvert) == false)
@@ -22,7 +24,34 @@ void ScalarConvert::convert(std::string toConvert)
         std::cout << "float: " << static_cast<float>(Ivalue) << ".0f" << '\n';
         std::cout << "double: " << static_cast<double>(Ivalue) << '\n';
     }
+    if (check_if_float(toConvert) == true)
+    {
+        if (conv >= 33 && conv <= 126)
+            std::cout << "char: " << static_cast<char>(conv) << '\n';
+        else
+            std::cout << "char: not displayable\n";
+        std::cout << "int: " << static_cast<int>(conv) << '\n';
+        std::cout << "float: " << toConvert << '\n';
+        std::cout << "double: " << conv << '\n';
+    }
 }
+
+bool    check_if_float(std::string str)
+{
+    int c = 0;
+    int f = 0;
+    for (int i = 0; str[i]; i++)
+    {
+        if (str[i] == '.')
+            c++;
+        if (str[i] == 'f')
+            f++;
+    }
+    if (c == 1 && f == 1)
+        return true;
+    return false;
+}
+
 
 bool    check_if_num(std::string str)
 {
