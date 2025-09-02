728d20c69b0f6504124c69317ee8c2fd1f46ddfd
ihhdj <ihebhdj42@gmail.com>
2025-09-02 14:21:07 +0200

    ex00 done

---
+++ cpp_08/ex00/main.cpp
@@ -0,0 +1,17 @@
+#include "easyfind.hpp"
+
+int main()
+{
+    std::list<int>  lst;
+
+    for (int i = 0; i < 10; i++)
+        lst.push_back(i + 1);
+    try
+    {
+        easyfind(lst, 56);
+    }
+    catch(const std::exception &e)
+    {
+        std::cout << "the number is not in the list\n";
+    }
+}
