0a6aaf1a67b8b3110826263aadb2003a2442cc33
ihhdj <ihebhdj21@gmail.com>
2025-09-03 14:49:17 +0200

    almost done ex01

--- cpp_08/ex01/main.cpp
+++ cpp_08/ex01/main.cpp
@@ -2,15 +2,19 @@
 
 int main()
 {
-    Span sp = Span(0);
+    Span sp = Span(10);
 
     try
     {
-        sp.addNumber(4);
-        sp.addNumber(3);
+        sp.addNumber(50);
+        sp.addNumber(100);
+        sp.addNumber(130);
+        sp.addNumber(1000);
+        std::cout << sp.shortestSpan() << '\n';
     }
     catch(const std::exception& e)
     {
-        std::cout << "error the array is full\n";
+        std::cout << e.what();
+        exit (1);
     }
 }
