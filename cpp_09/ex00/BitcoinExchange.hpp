6b4bb9064228e986cd24ee0748ff7b6b3e8709dc
ihhdj <ihebhdj42@gmail.com>
2025-09-05 11:25:03 +0200

    begin parsing

--- cpp_09/ex00/BitcoinExchange.hpp
+++ cpp_09/ex00/BitcoinExchange.hpp
@@ -1,15 +1,33 @@
 #ifndef BITCOINEXCHANGE_HPP
 #define BITCOINEXCHANGE_HPP
 
-
 #include <iostream>
 #include <vector>
+#include <exception>
+#include <fstream>
 
 class BTC
 {
     private:
 
-
     public:
+    BTC();
+    BTC(const BTC &src);
+    ~BTC();
+    BTC &operator=(const BTC &rhs);
+    void    parse(char *argv);
+
+    class ExceptionClass : public std::exception
+    {
+        private:
+        const char *_msg;
+
+        public:
+        ExceptionClass(const char *msg);
+        const char *what() const throw();
+    };
 };
+
+void    parse_date(std::string date, BTC::ExceptionClass parse_exception);
+void    parse_value(std::string value, BTC::ExceptionClass parse_exception);
 #endif
