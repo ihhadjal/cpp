ab28a3a754d6d376d642c62ad0ebc0f0ce7be0d6
ihhdj <ihebhdj21@gmail.com>
2025-09-08 13:20:30 +0200

    imposter syndrome

--- cpp_09/ex02/PmergeMe.cpp
+++ cpp_09/ex02/PmergeMe.cpp
@@ -10,15 +10,19 @@ Sort &Sort::operator=(const Sort &rhs){
 
     if (this != &rhs)
     {
-        //nothing to copy
+        this->_vct = rhs._vct;
+        this->_lst = rhs._lst;
     }
     return *this;
 }
 
-void    Sort::parse(char **argv)
+void    Sort::parseAndInit(char **argv)
 {
-    for (int i = 1; argv[i]; ++i)
+    for (int i = 1; argv[i]; i++)
     {
+        int num = atoi(argv[i]);
+        this->_vct.push_back(num);
+        this->_lst.push_back(num);
         for (int j= 0; argv[i][j]; j++)
         {
             if (argv[i][j] == '-')
@@ -33,5 +37,12 @@ void    Sort::parse(char **argv)
             }
         }
     }
-    
+    std::cout << "Before: ";
+    for (std::vector<unsigned int>::iterator it = this->_vct.begin(); it != this->_vct.end(); ++it)
+        std::cout << *it << ' ';
+    std::cout << std::endl;
+}
+void    Sort::mergeSort(char **argv)
+{
+    this->parseAndInit(argv);
 }
