16abe0921ba2e306e7f33769db20e01258713a82
ihhdj <ihebhdj42@gmail.com>
2025-09-10 14:32:21 +0200

    push de la honte

--- cpp_09/ex02/PmergeMe.cpp
+++ cpp_09/ex02/PmergeMe.cpp
@@ -1,48 +1,82 @@
 #include "PmergeMe.hpp"
 
-Sort::Sort(){}
+Sort::Sort() : count(0){}
 
-Sort::Sort(const Sort &src){*this = src;}
+Sort::Sort(const Sort &src) { *this = src; }
 
-Sort::~Sort(){}
+Sort::~Sort() {}
 
-Sort &Sort::operator=(const Sort &rhs){
+Sort &Sort::operator=(const Sort &rhs)
+{
 
     if (this != &rhs)
     {
+        this->count =  rhs.count;
         this->_vct = rhs._vct;
         this->_lst = rhs._lst;
     }
     return *this;
 }
 
-void    Sort::parseAndInit(char **argv)
+void Sort::parseAndInit(char **argv)
 {
     for (int i = 1; argv[i]; i++)
     {
         int num = atoi(argv[i]);
         this->_vct.push_back(num);
         this->_lst.push_back(num);
-        for (int j= 0; argv[i][j]; j++)
+        for (int j = 0; argv[i][j]; j++)
         {
             if (argv[i][j] == '-')
             {
                 std::cout << "Error: negative numbers are not allowed\n";
-                exit (1);
+                exit(1);
             }
             if (!isdigit(argv[i][j]) && argv[i][j] != '+')
             {
                 std::cout << "Error: int only are accepted\n";
-                exit (1);
+                exit(1);
             }
         }
     }
-    std::cout << "Before: ";
-    for (std::vector<unsigned int>::iterator it = this->_vct.begin(); it != this->_vct.end(); ++it)
-        std::cout << *it << ' ';
-    std::cout << std::endl;
 }
-void    Sort::mergeSort(char **argv)
+void Sort::mergeSort(char **argv)
 {
     this->parseAndInit(argv);
+    std::cout << "Before: ";
+    printContainer(this->_vct);
+    sort(this->_vct);
+    sort(this->_lst);
+}
+
+std::vector<int> Sort::generateJacobsthalSequence(int n)
+{
+    std::vector<int> jacobsthal;
+    std::vector<int> jacobsthalNumbers;
+
+    jacobsthalNumbers.push_back(0);
+    jacobsthalNumbers.push_back(1);
+
+    int current = 1;
+    while (current < n)
+    {
+        int next = jacobsthalNumbers[jacobsthalNumbers.size() - 1] + 2 * jacobsthalNumbers[jacobsthalNumbers.size() - 2];
+        jacobsthalNumbers.push_back(next);
+        current = next;
+    }
+
+    int lastAdded = 1;
+
+    for (size_t i = 2; i < jacobsthalNumbers.size() && jacobsthalNumbers[i] <= n; ++i)
+    {
+        int limit = jacobsthalNumbers[i];
+
+        for (int j = std::min(limit, n); j > lastAdded; --j)
+        {
+            jacobsthal.push_back(j);
+        }
+        lastAdded = limit;
+    }
+
+    return jacobsthal;
 }
--- cpp_09/ex02/PmergeMe.hpp
+++ cpp_09/ex02/PmergeMe.hpp
@@ -1,24 +1,177 @@
 #ifndef PMERGEME_HPP
 #define PMERGEME_HPP
 
-
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
 #include <deque>
+#include <algorithm>
+#include <ctime>
+#include <typeinfo>
 
 class Sort
 {
-    private:
+private:
     std::vector<unsigned int> _vct;
     std::deque<unsigned int> _lst;
+    int                      count;
 
-    public:
+    template <typename Container>
+    void fordJohnson(Container &cont);
+
+    template <typename Container>
+    typename Container::iterator binarySearch(Container &cont,
+                                              typename Container::iterator begin,
+                                              typename Container::iterator end,
+                                              const typename Container::value_type &value);
+
+    std::vector<int> generateJacobsthalSequence(int n);
+
+public:
     Sort();
     Sort(const Sort &src);
     ~Sort();
     Sort &operator=(const Sort &rhs);
-    void    parseAndInit(char **argv);
-    void    mergeSort(char **argv);
+    void parseAndInit(char **argv);
+    void mergeSort(char **argv);
+
+    template <typename Container>
+    void sort(Container &cont);
 };
+
+template <typename Container>
+void printContainer(Container &cont)
+{
+    typename Container::iterator it;
+    for (it = cont.begin(); it != cont.end(); ++it)
+        std::cout << *it << ' ';
+    std::cout << '\n';
+}
+
+template <typename Container>
+void Sort::sort(Container &cont)
+{
+    if (cont.size() <= 1)
+        return;
+    clock_t start = clock();
+    fordJohnson(cont);
+    clock_t end = clock();
+    if (count == 0)
+    {
+        std::cout << "After: ";
+        printContainer(cont);    
+    }
+    count ++;
+    double time = ((double)(end - start) / CLOCKS_PER_SEC) * 1000000;
+    if (typeid(Container) == typeid(std::vector<unsigned int>))
+        std::cout << "Time to process a range of " << cont.size() << " elements with std::vector : " << time << " us" << std::endl;
+    else
+        std::cout << "Time to process a range of " << cont.size() << " elements with std::deque : " << time << " us" << std::endl;
+}
+
+template <typename Container>
+void Sort::fordJohnson(Container &cont)
+{
+    if (cont.size() <= 1)
+        return;
+
+    std::vector<std::pair<typename Container::value_type, typename Container::value_type> > pairs;
+    typename Container::value_type straggler = 0;
+    bool hasStraggler = false;
+
+    for (size_t i = 0; i < cont.size(); i += 2)
+    {
+        if (i + 1 < cont.size())
+        {
+            typename Container::value_type a = cont[i];
+            typename Container::value_type b = cont[i + 1];
+            if (a > b)
+                pairs.push_back(std::make_pair(b, a));
+            else
+                pairs.push_back(std::make_pair(a, b));
+        }
+        else
+        {
+            straggler = cont[i];
+            hasStraggler = true;
+        }
+    }
+
+    Container largerElements;
+    for (size_t i = 0; i < pairs.size(); ++i)
+    {
+        largerElements.push_back(pairs[i].second);
+    }
+
+    if (largerElements.size() > 1)
+        fordJohnson(largerElements);
+
+    Container result;
+
+    if (!pairs.empty())
+        result.push_back(pairs[0].first);
+
+    for (typename Container::iterator it = largerElements.begin(); it != largerElements.end(); ++it)
+        result.push_back(*it);
+
+    std::vector<int> jacobsthal = generateJacobsthalSequence(pairs.size());
+    std::vector<bool> inserted(pairs.size(), false);
+
+    if (!pairs.empty())
+        inserted[0] = true;
+
+    for (size_t i = 0; i < jacobsthal.size(); ++i)
+    {
+        int idx = jacobsthal[i] - 1;
+        if (idx >= 0 && idx < static_cast<int>(pairs.size()) && !inserted[idx])
+        {
+            typename Container::value_type valueToInsert = pairs[idx].first;
+            typename Container::iterator pos = binarySearch(result, result.begin(), result.end(), valueToInsert);
+            result.insert(pos, valueToInsert);
+            inserted[idx] = true;
+        }
+    }
+
+    for (size_t i = 0; i < pairs.size(); ++i)
+    {
+        if (!inserted[i])
+        {
+            typename Container::value_type valueToInsert = pairs[i].first;
+            typename Container::iterator pos = binarySearch(result, result.begin(), result.end(), valueToInsert);
+            result.insert(pos, valueToInsert);
+        }
+    }
+
+    if (hasStraggler)
+    {
+        typename Container::iterator pos = binarySearch(result, result.begin(), result.end(), straggler);
+        result.insert(pos, straggler);
+    }
+
+    cont = result;
+}
+
+template <typename Container>
+typename Container::iterator Sort::binarySearch(Container &,
+                                                typename Container::iterator begin,
+                                                typename Container::iterator end,
+                                                const typename Container::value_type &value)
+{
+    typename Container::iterator left = begin;
+    typename Container::iterator right = end;
+
+    while (left != right)
+    {
+        typename Container::iterator mid = left;
+        std::advance(mid, std::distance(left, right) / 2);
+
+        if (*mid < value)
+            left = ++mid;
+        else
+            right = mid;
+    }
+
+    return left;
+}
+
 #endif
